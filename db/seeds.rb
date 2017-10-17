7509f89f9a690e53923fdbdefb8b37ba2b80a36b
rever0f <inkling@binkmail.com>
2017-10-17 01:07:42 -0400

    fix seeds typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-api_auth = ApiAuth.create([{ name: 'tvdb', auth: '' a}])
+api_auth = ApiAuth.create([{ name: 'tvdb', auth: ''}])
